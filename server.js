99e546a6ca85f5859e328bc88f56313e77cd4b34
Adrian Birta <adrianbirta@gmail.com>
2024-02-27 13:21:34 +0200

    Initial Commit Node.js Server

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class Emitter extends EventEmitter {}
+// initialize object
+const myEmitter = new Emitter();
+
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  const extension = path.extname(req.url);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// myEmitter.on('log', (msg) => {
+//   logEvents(msg);
+// });
+
+//   myEmitter.emit('log', 'Log event emitted!');
